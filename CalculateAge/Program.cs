a2d11bfd328a7764268ce5d90df43e03e9916f79
EvertonAraujo88 <evertonaraujosenai@gmail.com>
2024-04-10 16:53:28 -0300

    Adicionando primeiro teste, calculadora de idade em dias.

---
+++ CalculateAge/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine($"Informe sua idade expressa em anos: ");
+int years = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"em meses: ");
+int months = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"em dias: ");
+int days = int.Parse(Console.ReadLine());
+
+int ageDays = (years*365) + (months*30) + days;
+
+Console.WriteLine($" Sua idade em dias é {ageDays} dias.");
